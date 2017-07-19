90a099b00dac6ddb3f6a0f77e8815df1247ac8a9
justprosh <just.prosh@gmail.com>
2017-07-19 17:59:08 +0300

    fix for build gui apps

--- src/rtklib.h
+++ src/rtklib.h
@@ -253,10 +253,17 @@ extern "C" {
 #define MAXCOMMENT  10                  /* max number of RINEX comments */
 #define MAXSTRPATH  1024                /* max length of stream path */
 #define MAXSTRMSG   1024                /* max length of stream message */
+
+#ifdef WIN32
+#define MAXSTRRTK   8                   /* max number of stream in RTK server */
+#define MAXSOLRTK   2                   /* max number of solution streams in RTK server */
+#else
 #define MAXSTRRTK   10                  /* max number of stream in RTK server */
+#define MAXSOLRTK   4                   /* max number of solution streams in RTK server */
+#endif
+
 #define MAXSBSMSG   32                  /* max number of SBAS msg in RTK server */
 #define MAXSOLMSG   8191                /* max length of solution message */
-#define MAXSOLRTK   4                   /* max number of solution streams in RTK server */
 #define MAXRAWLEN   4096                /* max length of receiver raw message */
 #define MAXERRMSG   4096                /* max length of error/warning message */
 #define MAXANT      64                  /* max length of station name/antenna type */
